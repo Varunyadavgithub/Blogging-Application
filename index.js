8ecbbfae89e47f7c98bbb40a58f4626ac4ffa8c6
Varun Yadav <vy056038@gmail.com>
2024-08-17 13:52:35 +0530

    added .env file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user.routes");
@@ -6,12 +8,11 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCokie } = require("./middleware/authentication");
 const Blog = require("./models/blog.model");
-
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect("mongodb://localhost:27017/blogify")
+  .connect(process.env.MONGO_URL)
   .then((e) => console.log("Mongodb connected successfully"));
 
 app.set("view engine", "ejs");
@@ -20,13 +21,13 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCokie("token"));
-app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.resolve("./public")));
 
 app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
-    blogs:allBlogs,
+    blogs: allBlogs,
   });
 });
 
