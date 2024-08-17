cea1d966a0de612428c06630bb70334a155dc5f2
Varun Yadav <vy056038@gmail.com>
2024-08-17 13:38:18 +0530

    implemented the comment functionality

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -2,7 +2,8 @@ const { Router } = require("express");
 const router = require("./user.routes");
 const multer = require("multer");
 const path = require("path");
-const Blog=require("../models/blog.model");
+const Blog = require("../models/blog.model");
+const Comment = require("../models/comments.model");
 
 const route = Router();
 
@@ -25,14 +26,35 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
-  const {title, body}=req.body;
-  const blog= await Blog.create({
+  const { title, body } = req.body;
+  const blog = await Blog.create({
     body,
     title,
-    createdBy:req.user._id,
-    coverImageURL:`/uploads/${req.file.filename}`,
-  })
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
   return res.redirect(`/blog/${blog._id}`);
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 module.exports = router;
